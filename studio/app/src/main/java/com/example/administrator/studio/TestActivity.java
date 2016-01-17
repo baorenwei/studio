e66cb8afd505455f57425a550afd76ae0a690be4
baorenwei <15986773816@163.com>
2016-01-17 15:31:45 +0800

    TestActivity的TextView

--- studio/app/src/main/java/com/example/administrator/studio/TestActivity.java
+++ studio/app/src/main/java/com/example/administrator/studio/TestActivity.java
@@ -1,6 +1,7 @@
 package com.example.administrator.studio;
 
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Administrator on 2016/1/17.
@@ -8,4 +9,5 @@ import android.widget.Button;
 public class TestActivity {
 
     Button mBtn;
+    TextView mTextView;
 }
