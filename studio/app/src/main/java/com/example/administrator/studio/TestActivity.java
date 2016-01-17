b23de21429bbfeba8e077728ab29a1e99059a1f3
baorenwei <15986773816@163.com>
2016-01-17 14:35:45 +0800

    TestActivity 的Button

--- studio/app/src/main/java/com/example/administrator/studio/TestActivity.java
+++ studio/app/src/main/java/com/example/administrator/studio/TestActivity.java
@@ -1,7 +1,11 @@
 package com.example.administrator.studio;
 
+import android.widget.Button;
+
 /**
  * Created by Administrator on 2016/1/17.
  */
 public class TestActivity {
+
+    Button mBtn;
 }
